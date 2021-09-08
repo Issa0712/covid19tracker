9167bcadd63a90337aff08e90cf173055f274d8d
Issa0712 <cisse0712@gmail.com>
2021-09-08 11:15:06 +0100

    fetch url updated to work

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
 
   const onCountryChange = async (e) => {
       const selectedCountry = e.target.value
-      const url = selectedCountry === 'worldwide' ? 'https:disease.sh/v3/covid-19/all' : `https:disease.sh/v3/covid-19/countries/${selectedCountry}?strict=true`
+      const url = selectedCountry === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${selectedCountry}?strict=true`
 
       await fetch(url)
       .then (response => response.json())
